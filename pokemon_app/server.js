e1891ffabb1bd783f23099df5d7e185f036721f7
Courtney <courtneycook82@gmail.com>
2023-09-26 12:42:41 -0400

    Show view shows req.params.id

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -1,21 +1,30 @@
-// Import the Express.js module
 const express = require('express');
-
-// Create an instance of Express
 const app = express();
-
+const port = 3001;
+const jsxEngine = require('jsx-view-engine');
 const pokemon = require('./models/pokemon');
 
-// Define a GET route that sends a welcome message
-app.get('/', (req, res) => {
-  res.send('Welcome to the Pokemon App!');
-});
+
+app.set('view engine', 'jsx');
+app.engine('jsx', jsxEngine());
+//Middleware... near the top, around other app.use() calls
+app.use(express.urlencoded({extended:false}))
+
+app.use((req, res, next) => {
+    console.log('I run for all routes')
+    next()
+})
 
 app.get('/pokemon', (req, res) => {
-    res.render('Index');
-  });
+  // Render the 'Index' view (Index.jsx) and pass the 'pokemon' data
+  res.render('Index', { pokemon : pokemon });
+});
 
-// Start the server and listen on the specified port
-app.listen(3001, () => {
-    console.log('listening');
+app.get('/pokemon/:id', (req, res) => {
+  const { id } = req.params;
+  res.send(id);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
--- pokemon_app/views/Index.jsx
+++ pokemon_app/views/Index.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
-import pokemon from '../models/pokemon';
 
-// Inline styles as an object
 const myStyle = {
   color: '#ffffff',
   backgroundColor: '#000000',
 };
 
-class Index extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1 style={myStyle}>See All The Pokemon!</h1>
-        <ul>
-            {pokemon.map((pokemon, i) => {
-                return (
-                <li>
-                    The{' '}
-                    <a href={`/pokemon/${i}`}>
-                        {pokemon.name}
-                    </a>{' '}
-                    looks like {pokemon.img} 
-                </li>
-                );
-            })}
-        </ul>
-      </div>
-    );
-  }
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export default Index;
+class Index extends React.Component  {
+  render () {
+  const pokemon = this.props.pokemon
+  return (
+    <div style={myStyle}>
+      <h1>See All The Pokemon!</h1>
+      <ul>
+        {pokemon.map((poke, index) => (
+          <li key={index}>
+            <img src={poke.img} alt={poke.name} />
+            <p>Name: {capitalizeFirstLetter(poke.name)}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+}
+
+module.exports = Index;
